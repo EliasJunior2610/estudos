6f29c47afb61759b4dca5f2f5b6a5c367c368af5
EliasJunior2610 <e26junior2002@gmail.com>
2022-07-05 14:36:56 -0300

    Python POO

---
+++ Estudos/cursopythonyt/associacao.py
@@ -0,0 +1,14 @@
+from classes import Escritor
+from classes import Caneta
+from classes import MaquinaDeEscrever
+
+escritor = Escritor('Joãozinho')
+caneta = Caneta('Bic')
+maquina = MaquinaDeEscrever()
+
+escritor.ferramenta = maquina
+escritor.ferramenta.escrever()
+
+del escritor
+print(caneta.marca)
+maquina.escrever()
