73345c7e269fc14b26ef3f656c31e067bcbcbda1
EliasJunior2610 <e26junior2002@gmail.com>
2022-07-05 11:21:47 -0300

    Python POO

---
+++ Estudos/cursopythonyt/encapsulamento.py
@@ -0,0 +1,33 @@
+"""
+public, protected, private
+_ privado/protected (public _)
+__ privado (_NOMEDACLASSE__nomeatributo)
+"""
+
+
+class BaseDeDados:
+    def __init__(self):
+        self.__dados = {}
+
+    @property
+    def dados(self):
+        return self.__dados
+
+    def inserir_cliente(self, id, nome):
+        if 'clientes' not in self.__dados:
+            self.__dados['clientes'] = {id: nome}
+        else:
+            self.__dados['clientes'].update({id: nome})
+
+    def lista_clientes(self):
+        for id, nome in self.__dados['clientes'].items():
+            print(id, nome)
+
+    def apaga_cliente(self, id):
+        del self.__dados['clientes'][id]
+
+
+bd = BaseDeDados()
+bd.inserir_cliente(1, 'Otávio')
+bd.inserir_cliente(2, 'Miranda')
+print(bd.dados)
