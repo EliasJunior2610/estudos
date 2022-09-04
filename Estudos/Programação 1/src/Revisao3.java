ce08884c7dd843ebca625b3f096178e9b4ad7a44
EliasJunior2610 <e26junior2002@gmail.com>
2022-09-04 15:48:16 -0300

    Estudos

--- Estudos/Programação 1/src/Revisao3.java
+++ Estudos/Programação 1/src/Revisao3.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Revisao3 {
     public static void main(String[] args) {
+        // Listas:
         String listaMulheres[] = { "Rosimere", "Merielly", "Natália", "Tathyanne", "Máyra", "Cecília", "Maria",
                 "Karine", "Eliane", "Amanda", "Vanessa", "Júlia", "Grazielly", "Luísa", "Bárbara", "Larissa", "Sabrina",
                 "Nicole", "Rebeca", "Danielle" };
@@ -10,22 +13,77 @@ public class Revisao3 {
                 "Nícolas", "Miguel", "Samuel", "Juan", "Eduardo", "Ivo", "Breno", "Vitor", "Caio", "Ronaldo",
                 "Gustavo" };
         int idadeHomens[] = { 20, 19, 24, 40, 28, 37, 22, 40, 38, 32, 27, 36, 19, 40, 20, 19, 32, 22, 24, 33, 30, 29,
-                35, 24, 40, 23, 30, 29, 19, 20 };
+                35, 24, 20, 23, 30, 29, 19, 20 };
 
-        String primeirosIncritos[] = new String[7];
+        ArrayList<String> primeirosInscritos = new ArrayList<String>();
+        ArrayList<String> contratados = new ArrayList<String>();
+        ArrayList<String> maisProvaveisFeminino = new ArrayList<String>();
+        ArrayList<String> maisProvaveisMasculino = new ArrayList<String>();
+
+        // Questão 1:
         for (int i = 0; i < 7; i++) {
-            if (i < 3) {
-                primeirosIncritos[i] = listaMulheres[i];
-            } else {
-                primeirosIncritos[i] = listaHomens[i];
-            }
+            primeirosInscritos.add(listaMulheres[i]);
+            primeirosInscritos.add(listaHomens[i]);
         }
         System.out.print("Os primeiros sete inscritos são: ");
-        for (int i = 0; i < 7; i++) {
-            if (i < 6) {
-                System.out.print(primeirosIncritos[i] + ", ");
+        for (int i = 0; i < primeirosInscritos.size(); i++) {
+            if (i < (primeirosInscritos.size() - 1)) {
+                System.out.print(primeirosInscritos.get(i) + ", ");
             } else {
-                System.out.print(primeirosIncritos[i] + ".");
+                System.out.print(primeirosInscritos.get(i) + ".");
+            }
+        }
+        System.out.println();
+
+        // Questão 2:
+        for (int i = 0; i < 20; i++) {
+            if (idadeMulheres[i] >= 38) {
+                contratados.add(listaMulheres[i]);
+            }
+        }
+        for (int i = 0; i < 30; i++) {
+            if (idadeHomens[i] >= 38) {
+                contratados.add(listaHomens[i]);
+            }
+        }
+        System.out.print("Os mais prováveis de serem contratados são: ");
+        for (int i = 0; i < contratados.size(); i++) {
+            if (i < (contratados.size() - 1)) {
+                System.out.print(contratados.get(i) + ", ");
+            } else {
+                System.out.print(contratados.get(i) + ".");
+            }
+        }
+        System.out.println();
+
+        // Questão 3:
+        for (int i = 0; i < 20; i++) {
+            if (idadeMulheres[i] >= 35) {
+                maisProvaveisFeminino.add(listaMulheres[i]);
+            }
+        }
+        System.out.print("Os inscritos de sexo feminino com mais chance de serem contratados são: ");
+        for (int i = 0; i < maisProvaveisFeminino.size(); i++) {
+            if (i < (maisProvaveisFeminino.size() - 1)) {
+                System.out.print(maisProvaveisFeminino.get(i) + ", ");
+            } else {
+                System.out.print(maisProvaveisFeminino.get(i) + ".");
+            }
+        }
+        System.out.println();
+
+        // Questão 4:
+        for (int i = 0; i < 30; i++) {
+            if (idadeHomens[i] >= 35) {
+                maisProvaveisMasculino.add(listaHomens[i]);
+            }
+        }
+        System.out.print("Os inscritos de sexo masculino com mais chance de serem contratados são: ");
+        for (int i = 0; i < maisProvaveisMasculino.size(); i++) {
+            if (i < (maisProvaveisMasculino.size() - 1)) {
+                System.out.print(maisProvaveisMasculino.get(i) + ", ");
+            } else {
+                System.out.print(maisProvaveisMasculino.get(i) + ".");
             }
         }
     }
