c72f98940958b4037596bbf03e55533ff2349a2c
Elias Júnior <101825472+EliasJunior2610@users.noreply.github.com>
2022-11-16 19:38:01 -0300

    Delete Estudos/Projeto de Programação 1 directory

--- Estudos/Projeto de Programação 1/src/br/interfaces/IShow.java
+++
@@ -1,8 +0,0 @@
-package br.interfaces;
-
-public interface IShow {
-    //abre uma classe string para representar o nome da banda.
-    public String nomeDaBanda = "Mamonas Assassinas";
-//abre uma classe boolean, já que, deve se escolher entre dois valores, nesse caso se vai ou não ir para o camarote.
-    public boolean camarote = true;
-}
