1919a72784af44bea7fec8e8b0978fc74d54a8e6
EliasJunior2610 <e26junior2002@gmail.com>
2023-02-27 12:57:30 -0300

    Exercicio

---
+++ Programação 2/Torre de Hanói/src/Exercicio.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+public class Exercicio {
+    public static void main(String[] args) {
+        Stack<Integer> pilha1 = new Stack<>();
+        Stack<Integer> pilha2 = new Stack<>();
+        Stack<Integer> pilha3 = new Stack<>();
+        pilha1.push(3);
+        pilha1.push(2);
+        pilha1.push(1);
+        System.out.printf("Pilha 1: %s \n", pilha1);
+        System.out.printf("Pilha 2: %s \n", pilha2);
+        System.out.printf("Pilha 3: %s \n", pilha3);
+        torre(pilha1.size(), pilha1, pilha3, pilha2);
+    }
+
+    public static void torre(int i, Stack<Integer> pilha1, Stack<Integer> pilha3, Stack<Integer> pilha2) {
+        if (i> 0) {
+            torre(i-1, pilha1, pilha2, pilha3);
+            pilha3.push(pilha1.pop());
+            System.out.printf("Pilha 1: %s \n", pilha1);
+            System.out.printf("Pilha 2: %s \n", pilha2);
+            System.out.printf("Pilha 3: %s \n", pilha3);
+            torre(i-1, pilha2, pilha3, pilha1);
+        }
+    }
+}
